90da4f0b28413070023f6897444e13be91c6adc6
tarasmashtaler <tarasmash74@gmail.com>
2022-04-20 14:13:51 +0300

    corection lesson25

--- lessons/lesson_25/script.js
+++ lessons/lesson_25/script.js
@@ -3,39 +3,42 @@ let firstName = 'Taras';
 let fullName = 'Mashtaler';
 let yearOld = 47;
 let myCity = 'Ivano-frankivsk';
-alert('Привіт, ' + firstName);
+alert('Привіт, ' + firstName + '!');
 
 // Обчислення віку людини
 const yearNow = 2022;
 let yearBirthday = prompt('Привіт, який твій рік народження?');
-let result = (yearNow - yearBirthday);
+let result = yearNow - yearBirthday;
 alert('Твій вік: ' + result + ' років!'); 
 
 // Обчислення параметру квадрату
-let sideSquare = prompt('Вкажи довжину сторони квадрата і ти отримаєш його периметр:');
-let perimeterSquare = (sideSquare * 4);
+let sideSquare = prompt('Вкажи довжину сторони квадрата і ти отримаєш його периметр?');
+let perimeterSquare = sideSquare * 4;
+perimeterSquare = perimeterSquare.toFixed(2);
 alert('Периметер квадрата рівний: ' + perimeterSquare + ' сантиметрів.');
 
 // Обчислення площі круга
-let radiusCircle = prompt('Вкажи радіус кола і ти отримаєш площу його окружності:');
-let areaCircle = (Math.PI * Math.pow(radiusCircle, 2));
+let radiusCircle = prompt('Вкажи радіус кола і ти отримаєш площу його окружності?');
+let areaCircle = Math.PI * Math.pow(radiusCircle, 2);
 // другий спосіб
-// let areaCircle = (Math.PI * radiusCircle ** 2);
+// let areaCircle = Math.PI * radiusCircle ** 2;
 console.log(areaCircle);
-let areaCircleRound = areaCircle.toFixed();
-console.log(areaCircleRound);
-alert('Площа окружності кола рівна: ' + areaCircleRound + ' сантиметрів.');
+areaCircle = areaCircle.toFixed();
+console.log(areaCircle);
+alert('Площа окружності кола рівна: ' + areaCircle + ' сантиметрів.');
 
 // Відстань і швидкість між містами
-let distCity = prompt('Вкажи відстань між містами куди рухатимешся в кілометрах:');
-let distCityTime = prompt('Вкажи час за який плануєш проїхати цю відстань в годинах:');
-let distCitySpeed = (distCity / distCityTime);
-alert('Середня швидкість руху повинна бути не менше ' + distCitySpeed + 'км/год');
+let distCity = prompt('Вкажи відстань між містами куди рухатимешся в кілометрах?');
+let distCityTime = prompt('Вкажи час за який плануєш проїхати цю відстань в годинах?');
+let distCitySpeed = distCity / distCityTime;
+distCitySpeed = distCitySpeed.toFixed();
+alert('Середня швидкість руху повинна бути не менше ' + distCitySpeed + ' км/год');
 
 // Обмін валют
 const manyDollarEvro = 0.93;
 let manyDollar = prompt('Введіть суму в долларах:');
 let manyEvro = (manyDollarEvro * manyDollar);
+manyEvro = manyEvro.toFixed(2);
 alert('Після конвертації валют Ви отримаєте: ' + manyEvro + ' Evro');
 
 
