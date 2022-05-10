23c1943b92b1ee0f2e3baae72d58313190946204
tarasmashtaler <tarasmash74@gmail.com>
2022-05-10 11:11:56 +0300

    lesson33

--- lessons/lesson_33/script.js
+++ lessons/lesson_33/script.js
@@ -1,28 +1,28 @@
 // Створити HTML-сторінку для відображення/редагування тексту. При відкритті сторінки текст відображається за допомогою тега div. При натисканні Ctrl + E, замість div з'являється textarea з тим же текстом, який тепер можна редагувати. При натисканні Ctrl +, замість textarea з'являється div з уже зміненим текстом. Не забудь вимкнути поведінку за замовчуванням для цих поєднань клавіш.
 
-// document.addEventListener('keydown', (event) => {
-//   let divElement = document.getElementsByClassName('text')[0];
+document.addEventListener('keydown', (event) => {
+  let divElement = document.getElementsByClassName('text')[0];
 
-//   if (event.key === '+' && event.ctrlKey === true) {
-//     event.preventDefault();
-//     let textareaTag = document.getElementById("textareaID")
-//     let textareaContent = textareaTag.value
-//     divElement.innerHTML = textareaContent;
-//     console.log(textareaContent);
-//     divElement.hidden = false;
-//     textareaTag.hidden = true;
-//     return;
-//   }
-//   if (event.code === 'KeyE' && event.ctrlKey === true) {
-//     event.preventDefault();
-//     let divcontent = divElement.textContent;
-//     let textareaElement = "<textarea id='textareaID'>" + divcontent + "</textarea>";
-//     let element = document.getElementsByClassName('wrap')[0];
-//     element.insertAdjacentHTML('beforeend', textareaElement);
-//     divElement.hidden = true;
-//     return;
-//   }
-// });
+  if (event.key === '+' && event.ctrlKey === true) {
+    event.preventDefault();
+    let textareaTag = document.getElementById("textareaID")
+    let textareaContent = textareaTag.value
+    divElement.innerHTML = textareaContent;
+    console.log(textareaContent);
+    divElement.hidden = false;
+    textareaTag.hidden = true;
+    return;
+  }
+  if (event.code === 'KeyE' && event.ctrlKey === true) {
+    event.preventDefault();
+    let divcontent = divElement.textContent;
+    let textareaElement = "<textarea id='textareaID'>" + divcontent + "</textarea>";
+    let element = document.getElementsByClassName('wrap')[0];
+    element.insertAdjacentHTML('beforeend', textareaElement);
+    divElement.hidden = true;
+    return;
+  }
+});
 
 
 // Створити HTML-сторінку з великою таблицею. При кліку на заголовок стовпця, необхідно відсортувати дані цього стовпця. Врахуй, що числові значення повинні сортуватися як числа, а не як рядки.
@@ -31,7 +31,27 @@ const headingsValue = [];
 const mainContentValue = [];
 function sortTable(headingObject, index) {
 
+  deleteTableRows(document.querySelectorAll('tr.main-content'));
+
+  mainContentValue.sort(function (a, b) {
+    if (a.data[headingObject.name] < b.data[headingObject.name]) {
+      return 1;
+    }
+    if (a.data[headingObject.name] > b.data[headingObject.name]) {
+      return -1;
+    }
+  });
+  let tableElement = document.querySelector('table tbody');
+  mainContentValue.forEach((element) => {
+    tableElement.insertAdjacentHTML('beforeend', element.html.outerHTML);
+  });
 }
+function deleteTableRows(rowsArray) {
+  rowsArray.forEach((element) => {
+    element.remove();
+  });
+}
+
 function getHeadings() {
   headings.forEach((element, index) => {
     let headingObject = {
@@ -49,14 +69,21 @@ function getTableInfo() {
 
   tableRows.forEach((element, index) => {
     let infoItems = element.querySelectorAll('td');
-    console.log(infoItems);
-  });
+    let trObject = {
+      html: element,
+      data: {}
+    };
+    infoItems.forEach((td, column) => {
+      trObject.data[headingsValue[column].name] = td.innerText;
 
-  // console.log(tableRows);
+    });
+    mainContentValue.push(trObject);
+  });
 }
-// console.log(headingsValue);
 getHeadings();
 getTableInfo();
+
+
 
 
 
