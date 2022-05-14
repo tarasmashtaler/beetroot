c5fbe9cb95f86aa0576ae95c15c0d6114292cfff
tarasmashtaler <tarasmash74@gmail.com>
2022-05-14 07:45:04 +0300

    lesson39

---
+++ lessons/lesson_35/index.js
@@ -0,0 +1,38 @@
+
+const getCurrentWeather = () => {
+    return '+' + Math.floor((Math.random() * 10) + 1) + 'C';
+  }
+
+const setNewWeatherInDom = () => {
+  const storageWeatherDate = localStorage.getItem('weather-date');
+
+  if (hoursDifference(new Date(), new Date(storageWeatherDate)) < 2) {
+    const storageWeather = localStorage.getItem('weather');
+
+    updateWeatherInDom(storageWeather);
+    return;
+  } else {
+    const currentWeather = getCurrentWeather();
+    
+    updateWeatherInDom(currentWeather);
+    setWeatherInStorage(currentWeather);
+  }
+}
+const setWeatherInStorage = (weather) => {
+  localStorage.setItem('weather', weather);
+  localStorage.setItem('weather-date', new Date());
+}
+
+function hoursDifference(dt2, dt1) {
+  let diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= (60 * 60);
+  return Math.abs(Math.round(diff));
+}
+
+const updateWeatherInDom = (weather) => {
+  const weatherElement = document.getElementById('js-weather');
+  weatherElement.innerText = weather;
+}
+
+setNewWeatherInDom();
+
