c838e65c62dbf3e31cb4fbc7008c9ca962a1f443
tarasmashtaler <tarasmash74@gmail.com>
2022-06-17 21:11:14 +0300

    lesson46

---
+++ lessons/lesson_46/my-first-react-app/src/App.jsx
@@ -0,0 +1,24 @@
+import './app.scss';
+import Header from './components/header/Header.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+
+      {/* <header className="App-header"> */}
+        
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      {/* </header> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ lessons/lesson_46/my-first-react-app/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import styles from './header.module.scss';
+
+const Header = () => {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <h1>ВСЕ БУДЕ УКРАЇНА!!!</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
